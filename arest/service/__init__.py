18a2d071887d48b916c24ded949292347a866e50
max-orlov <maxim@gigaspaces.com>
2017-11-28 17:48:38 +0200

    simplified the registration process

--- arest/service/__init__.py
+++ arest/service/__init__.py
@@ -2,17 +2,31 @@
 from flask import Flask
 
 from . import (
-    core,
     execution,
     plugin,
     service,
     service_template,
-    task,
-    log,
+    core
 )
 
 app = Flask(__name__)
 
-for entity in (core, execution, plugin, service, service_template, task, log):
-    endpoint = '/{0}'.format(entity.handler.__endpoint__)
-    app.route(endpoint, methods=entity.handler.methods)(entity.handler)
+
+def register_api(**app_kwargs):
+    app.config.update(**app_kwargs)
+    for entity in (execution, plugin, service, service_template, core):
+        endpoints = (
+            entity.handler.__endpoint__
+            if isinstance(entity.handler.__endpoint__, (list, tuple)) else
+            (entity.handler.__endpoint__,)
+        )
+        for endpoint_suffix in endpoints:
+            endpoint = '/{0}'.format(endpoint_suffix)
+            app.route(endpoint, methods=entity.handler.methods)(entity.handler)
+
+
+
+register_api(
+    SQLALCHEMY_DATABASE_URI='sqlite://///home/maxim-pcu/.aria/models/db.sqlite',
+    SQLALCHEMY_TRACK_MODIFICATIONS=True
+)
